a00ef4602d4e93c488216b04bef1a8b709e48946
Angelo Di Marco <adi-marc@c1r9s1.42luxembourg.lu>
2026-04-21 14:03:53 +0200

    landing components set

--- front/src/app/landing/landing.ts
+++ front/src/app/landing/landing.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Header } from './header/header';
+import { Hero } from './hero/hero';
+import { Features } from './features/features';
+import { Carousel } from './carousel/carousel';
+import { Cta } from './cta/cta';
+import { Faq } from './faq/faq';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [
+    Header,
+    Hero,
+    Features,
+    Carousel,
+    Cta,
+    Faq,
+    Footer
+  ],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
---
+++ front/src/app/signup/sign-form/sign-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-form',
+  imports: [],
+  templateUrl: './sign-form.html',
+  styleUrl: './sign-form.scss',
+})
+export class SignForm {}
