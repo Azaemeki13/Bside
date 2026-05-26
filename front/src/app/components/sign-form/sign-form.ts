0c523f590831189587af9c4424420071251b6848
chloé sauvage <chloe.sauvage664@gmail.com>
2026-05-26 14:10:49 +0200

    mac myver angular

--- front/src/app/components/sign-form/sign-form.ts
+++ front/src/app/components/sign-form/sign-form.ts
@@ -1,22 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NavButton } from '../nav-button/nav-button'
 import { Router } from '@angular/router';
 import { LucideAngularModule, Mail, KeyRound, UserRound } from 'lucide-angular';
 import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-form',
   standalone: true,
-  imports: [LucideAngularModule, ReactiveFormsModule, NavButton],
+  imports: [LucideAngularModule, ReactiveFormsModule, CommonModule],
   templateUrl: './sign-form.html',
   styleUrl: './sign-form.scss',
 })
 export class SignForm {
+  isLoading = false;
   protected readonly mail = Mail;
   protected readonly keyRound = KeyRound;
   protected readonly userRound = UserRound;
-
   private authService = inject(AuthService);
   private router = inject(Router);
   private fb = inject(FormBuilder);
@@ -25,19 +25,19 @@ export class SignForm {
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]]
   });
-  protected errorMessage= '';
+  protected errorMessage = '';
   protected onSubmit(): void {
-    if (this.registerForm.invalid) {
-      return;
-    }
+    if (this.registerForm.invalid) return;
+    this.isLoading = true;
+    this.errorMessage = '';
     const formValues = this.registerForm.getRawValue();
     this.authService.registerAndLogin(formValues).subscribe({
       next: () => {
-        console.log("Registration and login successful !");
         this.authService.loadUserProfile();
         this.router.navigate(['/bside_app/home']);
       },
       error: (err) => {
+        this.isLoading = false;
         this.errorMessage = 'Registration failed. Email might already be in use.';
         console.error(err);
       }
